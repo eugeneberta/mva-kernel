bc9f393fc99725f4539f36ded310ae7b2be6b9a0
Eugène Berta <eugene.berta@telecom-paris.fr>
2023-04-03 18:56:18 +0200

    solved weisfeiler_lehman bugs

--- utils.py
+++ utils.py
@@ -213,43 +213,34 @@ def WL(Graph, max_iter, verbose=False) :
 
     return feature_vector
 
-def weisfeiler_lehman(G, h, normalize=False):
+def weisfeiler_lehman(G, h):
     """
     Implements the Weisfeiler-Lehman algorithm to compute a feature vector
     describing a graph in input.
-    
+
     Parameters:
     G (nx.Graph): Input graph
     h (int): Number of iterations of the algorithm
-    
+
     Returns:
     patterns (dict): Feature vector of the input graph
     """
     N = G.number_of_nodes()
-    
+
     # Initialize feature vectors
     node_features = np.zeros((h, N), dtype=object)
-    
+
     patterns = {}
-    if normalize:
-        for n in range(N):
-            pattern = str(G.nodes[n]['labels'][0])
-            if pattern in patterns:
-                patterns[pattern] += 1/N
-            else:
-                patterns[pattern] = 1/N
-            node_features[0, n] = pattern
-    else:
-        for n in range(N):
-            pattern = str(G.nodes[n]['labels'][0])
-            if pattern in patterns:
-                patterns[pattern] += 1
-            else:
-                patterns[pattern] = 1
-            node_features[0, n] = pattern
-    
+    for n in range(N):
+        pattern = str(G.nodes[n]['labels'][0])
+        if pattern in patterns:
+            patterns[pattern] += 1
+        else:
+            patterns[pattern] = 1
+        node_features[0, n] = pattern
+
     num_patterns = len(patterns)
-    
+
     # Iterate h times
     for i in range(1, h):
         step_patterns = {}
@@ -259,12 +250,12 @@ def weisfeiler_lehman(G, h, normalize=False):
             pattern = str(node_features[i-1, n]) + '>' + ''.join([str(f) for f in sorted_features])
 
             if pattern in step_patterns:
-                step_patterns[pattern] += (1+h)/N
+                step_patterns[pattern] += 1
             else:
-                step_patterns[pattern] = (1+h)/N
-            
+                step_patterns[pattern] = 1
+
             node_features[i, n] = pattern
-        
+
         if len(step_patterns) == num_patterns:
             return patterns
         else:
