a6cbb2ab17b1de386cb2b43b3d86405afecb174a
Eugène Berta <eugene.berta@telecom-paris.fr>
2023-04-09 19:37:11 +0200

    removed duplicata WL

--- utils.py
+++ utils.py
@@ -62,7 +62,6 @@ class KernelSVC:
     def predict(self, K):
         return K@np.multiply(self.alpha, self.y) + self.b
 
-
 def stratified_cross_val(
         G_train,
         train_labels,
@@ -164,8 +163,18 @@ def sorted_neighbors_id(G, vertex_id, pattern_dict) :
 
     return neighbors_dict_sorted
 
-def WL(Graph, max_iter, verbose=False) :
+def WL(Graph, max_iter, verbose=False):
+    """
+    Implements the Weisfeiler-Lehman algorithm to compute a feature vector
+    describing a graph in input.
 
+    Parameters:
+    G (nx.Graph): Input graph
+    max_iter (int): Number of iterations of the algorithm (first iteration not included. max_iter = h-1).
+
+    Returns:
+    patterns (dict): Feature vector of the input graph
+    """
     G = copy.deepcopy(Graph)
     
     feature_vector = {}
@@ -225,58 +234,6 @@ def WL(Graph, max_iter, verbose=False) :
             print('Feature_vector', feature_vector)
 
     return feature_vector
-
-def weisfeiler_lehman(G, h):
-    """
-    Implements the Weisfeiler-Lehman algorithm to compute a feature vector
-    describing a graph in input.
-
-    Parameters:
-    G (nx.Graph): Input graph
-    h (int): Number of iterations of the algorithm
-
-    Returns:
-    patterns (dict): Feature vector of the input graph
-    """
-    N = G.number_of_nodes()
-
-    # Initialize feature vectors
-    node_features = np.zeros((h, N), dtype=object)
-
-    patterns = {}
-
-    for n in range(N):
-        pattern = str(G.nodes[n]['labels'][0])
-        if pattern in patterns:
-            patterns[pattern] += 1
-        else:
-            patterns[pattern] = 1
-        node_features[0, n] = pattern
-
-    num_patterns = len(patterns)
-
-    # Iterate h times
-    for i in range(1, h):
-        step_patterns = {}
-        for n in range(N):
-            neighbor_features = [node_features[i-1, j] for j in G.neighbors(n)]
-            sorted_features = sorted(neighbor_features)
-            pattern = str(node_features[i-1, n]) + '>' + ''.join([str(f) for f in sorted_features])
-
-            if pattern in step_patterns:
-                step_patterns[pattern] += 1
-            else:
-                step_patterns[pattern] = 1
-
-            node_features[i, n] = pattern
-
-        if len(step_patterns) == num_patterns:
-            return patterns
-        else:
-            num_patterns = len(step_patterns)
-            patterns.update(step_patterns)
-
-    return patterns
 
 def WLK_linear(l1, l2):
     common_patterns = l1.keys() & l2.keys()
